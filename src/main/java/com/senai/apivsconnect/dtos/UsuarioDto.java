1fd4d5765287f159ea84b1d35442c4cb4b033801
Felipe <felipedefreitas177@gmail.com>
2023-10-27 20:16:22 -0300

    Desenvolvimento da api vsconnevt.

---
+++ src/main/java/com/senai/apivsconnect/dtos/UsuarioDto.java
@@ -0,0 +1,24 @@
+package com.senai.apivsconnect.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.font.MultipleMaster;
+
+public record UsuarioDto(
+        @NotBlank String nome,
+
+        @NotBlank @Email(message = "O email deve estar no formato válido") String email,
+
+        @NotBlank String senha,
+
+        String endereco,
+
+        String cep,
+
+        String tipo_usuario,
+
+        MultipartFile imagem
+) {
+}
